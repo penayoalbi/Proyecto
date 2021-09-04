51d7dfd393452c7abed5f03d652d536c8621d0aa
albi.penayo <albi.penayo@gmail.com>
2021-09-03 23:54:22 -0300

    editado

--- src/ferreteria/ControllerPersona.java
+++
@@ -1,4 +0,0 @@
-package ferreteria;
-
-public class ControllerPersona {
-}
--- src/ferreteria/bd.java
+++ src/ferreteria/bd.java
@@ -6,9 +6,7 @@ public class bd {
     static String conbd="jdbc:mysql://127.00.1:3306/ferreteria?user=root&password&serverTimezone=UTC";
     private ResultSet rs;
 
-    //realizar consulta/SELECT
     public ResultSet Consultar(String stringsql) {
-        
         try{
             Connection conn = DriverManager.getConnection(conbd);
             Statement statement = conn.createStatement();
@@ -17,27 +15,25 @@ public class bd {
             //conn.close();
         } catch (SQLException e) {
             System.out.println("ERROR: " + e.getMessage());
-          //  rs=null;
         }
         return rs;
     }
-    //guardar/INSERT
+
     //INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `clave`) VALUES (NULL, 'ana', 'sample@mail.com', '1234');
     public void Guardar(String stringsql) {//guardar registo en bd
         try {
             Connection conn = DriverManager.getConnection(conbd);
             Statement statement = conn.createStatement();
             statement.execute(stringsql);
-            statement.close();
-            conn.close();
+           // statement.close();
+          //  conn.close();
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
     //UPDATE `usuarios` SET `clave` = 'lalo123a4' WHERE `usuarios`.`id` = 2
-    public void modificardatos(String stringsql)
-    {//modificar registos
+    public void modificardatos(String stringsql) {
         try {
             Connection conn = DriverManager.getConnection(conbd);
             Statement ps = conn.createStatement();
@@ -48,13 +44,13 @@ public class bd {
     }
 
     //delete
-    public static int eliminar(String sentencia)
+    public static int eliminar(int item)
     {
-        try {
+        try {// "DELETE FROM `usuarios` WHERE `usuarios`.`usuarioID` = 3"?
             Connection conn = DriverManager.getConnection(conbd);
-            //String sentencia= "DELETE FROM beltran.Abmprofesor WHERE idprofesor = ? ";
+            String sentencia= "DELETE FROM `usuarios` WHERE `usuarios`.`usuarioID` = ? ";
             PreparedStatement ps = conn.prepareStatement(sentencia);
-            ps.setString(1,sentencia);
+            ps.setString(1,String.valueOf(item));
             return ps.executeUpdate();//retorna un valor entero
         }catch (SQLException e){
             System.out.println("Error"+e.getMessage());
