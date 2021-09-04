68679568704466fcc57835719ff4f35b7a31cba1
albi.penayo <albi.penayo@gmail.com>
2021-09-03 23:43:31 -0300

    validacion class

---
+++ src/ferreteria/validacion.java
@@ -0,0 +1,17 @@
+package ferreteria;
+
+import javafx.fxml.FXML;
+
+public class validacion {
+
+    @FXML
+    public boolean esString(String string){
+        return true;
+    }
+    @FXML
+    public boolean Vacio(String string){
+        return true;
+    }
+
+}
+
