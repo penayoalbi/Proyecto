8fb1df47662aad8766c6575c52d9f74528a274ee
albi.penayo <albi.penayo@gmail.com>
2021-09-10 01:47:09 -0300

    se agregó evento Click y listar

--- src/ferreteria/Producto.java
+++ src/ferreteria/Producto.java
@@ -1,10 +1,82 @@
 package ferreteria;
 
 public class Producto {
-    private int productoID;
+
+    private Integer productoID;
     private String nombre;
     private String descripcion;
     private float precioLista;
     private float precioVenta;
-    private int stock;
+    private Integer stock;
+    private String categoria;
+    private Integer proveedor;
+
+    //construct
+    public Producto(Integer productoID, String nombre, String descripcion, float precioLista, float precioVenta, Integer stock, String categoria, Integer proveedor) {
+        this.productoID = productoID;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precioLista = precioLista;
+        this.precioVenta = precioVenta;
+        this.stock = stock;
+    }
+
+    public Integer getProductoID() {
+        return productoID;
+    }
+
+    public void setProductoID(Integer productoID) {
+        this.productoID = productoID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecioLista() {
+        return precioLista;
+    }
+
+    public void setPrecioLista(float precioLista) {
+        this.precioLista = precioLista;
+    }
+
+    public float getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(float precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+    public void setCategoria(String categoria){
+        this.categoria=categoria;
+    }
+    public String getCategoria(){
+        return categoria;
+    }
+    public void setProveedor(Integer proveedor){
+        this.proveedor=proveedor;
+    }
+    public Integer getProveedor(){
+        return proveedor;
+    }
 }
--- src/ferreteria/productoController.java
+++ src/ferreteria/productoController.java
@@ -1,17 +1,46 @@
 package ferreteria;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class productoController {
+    @FXML private TableView <Producto>tablaProducto;
+    @FXML private TextField txtProductoID;
+    @FXML private TextField txtNombre;
+    @FXML private TextField txtDescripcion;
+    @FXML private TextField txtPrecioLista;
+    @FXML private TextField txtPrecioVenta;
+    @FXML private TextField txtStock;
+    @FXML private ComboBox cmbCategoria;
+    @FXML private ComboBox cmbProveedor;
 
+    @FXML private TableColumn <Producto,Integer> colProductoID ;
+    @FXML private TableColumn <Producto,String> colNombre ;
+    @FXML private TableColumn <Producto,String> colDescripcion;
+    @FXML private TableColumn <Producto,Float> colPrecioLista;
+    @FXML private TableColumn <Producto,Float> colPrecioVenta;
+    @FXML private TableColumn <Producto,Integer> colStock;
+    @FXML private TableColumn <Producto,String> colCategoria;
+    @FXML private TableColumn <Producto,String> colProveedor;
+
+    ObservableList<Producto> oblist = FXCollections.observableArrayList();//lista observable
+
+    bd base = new bd();
 
     @FXML
     public void controladorClose() {
@@ -35,13 +64,58 @@ public class productoController {
 
     @FXML
     public void initialize(){
+        eventoClick();
     }
-
 
     @FXML
     public void listar(){
         System.out.println("click en lista");
+        ResultSet rs;
+        try{
+            //SELECT `ProductoID`, `Nombre`, `Descripcion`, `PrecioLista`, `PrecioVenta`,
+            // `Stock`, `Categoria`, `proveedorID` FROM `productos` WHERE 1
+            rs=base.Consultar("SELECT `ProductoID`, `Nombre`, `Descripcion`, `PrecioLista`, `PrecioVenta`,`Stock`, `Categoria`, `proveedorID` FROM `productos` WHERE 1");
+            while(rs.next()){
+                oblist.add(new Producto(rs.getInt("ProductoID"),rs.getString("Nombre"),
+                        rs.getString("Descripcion"),rs.getFloat("PrecioLista"),
+                        rs.getFloat("PrecioVenta"),rs.getInt("Stock"),rs.getString("Categoria"),
+                        rs.getInt("proveedorID")));
+            }
+            colProductoID.setCellValueFactory(new PropertyValueFactory<>("ProductoID"));
+            colNombre.setCellValueFactory(new PropertyValueFactory<>("Nombre"));
+            colDescripcion.setCellValueFactory(new PropertyValueFactory<>("Descripcion"));
+            colPrecioLista.setCellValueFactory(new PropertyValueFactory<>("PrecioLista"));
+            colPrecioVenta.setCellValueFactory(new PropertyValueFactory<>("PrecioVenta"));
+            colStock.setCellValueFactory(new PropertyValueFactory<>("Stock"));
+            colCategoria.setCellValueFactory(new PropertyValueFactory<>("Categoria"));
+            colProveedor.setCellValueFactory(new PropertyValueFactory<>("proveedorID"));
+            tablaProducto.setItems(oblist);
+            tablaProducto.refresh();
+
+        }catch (Exception e){
+            System.out.println("Error en listar"+e.getMessage());
+        }
+
     }
+
+    @FXML
+    public void eventoClick(){
+        tablaProducto.getSelectionModel().selectedItemProperty().addListener((observableValue, valorAnterior, valorActual) ->{
+            txtProductoID.setText(String.valueOf(valorActual.getProductoID()));
+            txtNombre.setText(String.valueOf(valorActual.getNombre()));
+            txtDescripcion.setText(String.valueOf(valorActual.getDescripcion()));
+            txtPrecioLista.setText(String.valueOf(valorActual.getPrecioLista()));
+            txtPrecioVenta.setText(String.valueOf(valorActual.getPrecioVenta()));
+            txtStock.setText(String.valueOf(valorActual.getStock()));
+            tablaProducto.refresh();
+
+        } );
+    }
+
+    /*
+    * @FXML
+    *
+    * */
     @FXML
     public void modificar(){
         System.out.println("click en modificar");
@@ -57,10 +131,18 @@ public class productoController {
     @FXML
     public void nuevo(){
         System.out.println("click en nuevo");
+        limpiarCeldas();
     }
 
     @FXML
     public void limpiarCeldas(){
-        System.out.println("click en limpiarCeldas");
+        txtProductoID.setText(null);
+        txtNombre.setText(null);
+        txtDescripcion.setText(null);
+        txtPrecioLista.setText(null);
+        txtPrecioVenta.setText(null);
+        txtStock.setText(null);
+        cmbCategoria.setValue(null);
+        cmbProveedor.setValue(null);
     }
 }
