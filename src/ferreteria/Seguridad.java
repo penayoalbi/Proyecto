c99a0ba1c93db2ed68b1d733fa4067c5ec0575d5
albi.penayo <albi.penayo@gmail.com>
2021-09-19 22:48:55 -0300

    métodos encriptar y desencriptar ok

--- src/ferreteria/Seguridad.java
+++ src/ferreteria/Seguridad.java
@@ -1,29 +1,50 @@
 package ferreteria;
-
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import com.mysql.cj.jdbc.Blob;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.Base64;
+import static javax.crypto.Cipher.DECRYPT_MODE;
 import static javax.crypto.Cipher.ENCRYPT_MODE;
 
 public class Seguridad {
-    public static String hash(String clave) throws NoSuchAlgorithmException {
-        java.security.MessageDigest md = java.security.MessageDigest.getInstance("SHA-1");
-        byte[] array = md.digest(clave.getBytes());
-        StringBuilder cadena_a_hash = new StringBuilder();
-        for (int i = 0; i < array.length; ++i) {
-            cadena_a_hash.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+    //encode
+    public String Encriptar(String secretKey, String cadena){
+        String encriptado = "";
+        try{
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] pass = md5.digest(secretKey.getBytes("utf-8"));
+            byte[] byteskey = Arrays.copyOf(pass, 24);
+            SecretKey secretKey1 = new SecretKeySpec(byteskey, "DESede");
+            Cipher cifrado = Cipher.getInstance("DESede");
+            cifrado.init(ENCRYPT_MODE, secretKey1);
+            byte[] textoPlano = cadena.getBytes("utf-8"); //transformar en cadena de bytes a lo que recibo por parametro
+            byte[] bufer = cifrado.doFinal(textoPlano);//contiene el cifrado
+            byte[] base64Bytes = Base64.getEncoder().encode(bufer);//paso al arreglo bufer
+            encriptado =new String(base64Bytes);
+        }catch(Exception e){
+            System.out.println("Error en encriptar");
         }
-        return cadena_a_hash.toString();
+        return encriptado;
     }
 
-    //método desencriptar
-    public static String desencriptar(String a_desencriptar){
+    //decode
+    public static String Desencriptar(String mykey, String encriptado){
+        String desencriptado = "";
         try{
-            return a_desencriptar;
+            byte[] msj = Base64.getDecoder().decode(encriptado.getBytes("utf-8"));//arreglo de bytes
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] decPass = md5.digest(mykey.getBytes("utf-8"));//
+            byte[] keyBytes = Arrays.copyOf(decPass,24);
+            SecretKey newkey = new SecretKeySpec(keyBytes, "DESede");//
+            Cipher decipher = Cipher.getInstance("DESede");
+            decipher.init(DECRYPT_MODE, newkey);
+            byte[] textoPlano = decipher.doFinal(msj);
+            desencriptado = new String(textoPlano, "UTF-8");
         }catch (Exception e){
-            System.out.println("Error al desencriptar: "+e.getMessage());
-            String msjError ="ocurrio un error al encriptar: " + e.getMessage();
-            return msjError;
+            System.out.println("Error en desencriptar.");
         }
+        return desencriptado;
     }
 }
