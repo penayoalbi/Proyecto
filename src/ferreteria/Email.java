13efe751996392e6666c8a25c853a31d67626341
albi.penayo <albi.penayo@gmail.com>
2021-09-21 23:55:57 -0300

    Revisar MimeMessage

--- src/ferreteria/Email.java
+++ src/ferreteria/Email.java
@@ -1,44 +1,49 @@
 package ferreteria;
 
+import java.io.IOException;
 import java.net.PasswordAuthentication;
 import java.net.SocketAddress;
 import java.util.Properties;
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
+import javax.mail.internet.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.search.SearchTerm;
-
 
 
 public class Email {
 
     public void enviarMail(String destinatario, String claveGenerada) throws MessagingException {
+
         Properties propiedad = new Properties();
-        propiedad.setProperty("mail.smtp.host", "smtp gmail com");
+        propiedad.setProperty("mail.smtp.host", "smtp.gmail.com");
         propiedad.setProperty("mail.smtp.starttls.enable", "true");
         propiedad.setProperty("mail.smtp.port", "587");
         propiedad.setProperty("mail.smtp.auth", "true");
 
         Session session = Session.getDefaultInstance(propiedad);
-        String correo = "albi.penayo@gmail.com";
-        String pass = "cr7carplove2911.";
-        String destino = destinatario;
-        String asunto = "Recupero de contraseña";
-        String mensajeClave = claveGenerada; //mando el mensaje que autogenero con mi sistema
+        String correo = "ferreteriagamarra812@gmail.com";
+        String pass = "ferreteria812";
+        String asunto = "Recupero de contraseña";//mando el mensaje que  se genero
 
         MimeMessage mail = new MimeMessage(session);
-        mail.setFrom(new InternetAddress(correo));//desde donde se envia
-        mail.addRecipients(javax.mail.Message.RecipientType.TO, String.valueOf(new InternetAddress(destinatario)));
-        mail.setSubject(asunto);
-        mail.setText(mensajeClave);
+        try {
+            mail.setFrom(new InternetAddress(correo));//desde donde se envia
+            mail.addRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress(destinatario)});
+            mail.setSubject(asunto);
+            mail.setText("Su clave de recuperacion es: " + claveGenerada);
 
-        Transport transport = session.getTransport("smtp");
-        transport.connect(correo, pass);
-        transport.sendMessage(mail, mail.getRecipients(javax.mail.Message.RecipientType.TO));
-        transport .close();
+            Transport transport = session.getTransport("smtp");
+            transport.connect(correo, pass);
+            //   transport.sendMessage(mail, mail.getRecipients(javax.mail.Message.RecipientType.TO));
+            transport.sendMessage(mail, mail.getAllRecipients());
+            transport.close();
+        } catch (AddressException e) {
+            System.out.println("error en enviar correo: " + e.getMessage());
+        } catch (MessagingException e) {
+            System.out.println("error en enviar correo: " + e.getMessage());
+        }
     }
-
 }
