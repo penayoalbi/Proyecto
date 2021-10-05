6bdb01090393da442fcbf3563a4531e53ba3e0ea
albi.penayo <albi.penayo@gmail.com>
2021-10-05 01:24:31 -0300

    editado

--- src/ferreteria/Controller.java
+++ src/ferreteria/Controller.java
@@ -10,85 +10,70 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
-import java.net.PasswordAuthentication;
 import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Controller {
-    private static final int WITDH =0 ;
-    @FXML private TextField txtUsuario;
-    @FXML private TextField txtClave;
-    @FXML private Button btnIngresar;
-    @FXML private Button btnSalir;
-    @FXML private AnchorPane Home, page2, page3, page4;
-    @FXML private Hyperlink recuperacion;
+    private static final int WITDH = 0;
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private TextField txtClave;
+    @FXML
+    private Button btnIngresar;
+    @FXML
+    private Button btnSalir;
+    @FXML
+    private AnchorPane Home, page2, page3, page4;
+    @FXML
+    private Hyperlink recuperacion;
 
-@FXML
-    void ingresar( ActionEvent event) throws Exception {
-    System.out.println("Click en el boton ingresar");
-    bd base = new bd();//instancia de bd
+    @FXML
+    void ingresar(ActionEvent event) throws Exception {
+        System.out.println("Click en el boton ingresar");
+        bd base = new bd();//instancia de bd
 
-    if (txtUsuario.getText().equals("") || txtClave.getText().equals("")) {
-        alert("llene los campos de usuario y clave");
-    } else {
-        ResultSet rs;
-        rs = base.Consultar("SELECT * FROM ferreteria.usuarios");
-        int resu = 0;
-        while (rs.next()){
-            if (txtUsuario.getText().equals(rs.getString("nombre")) && txtClave.getText().equals(rs.getString("clave"))) {
-                // System.out.println("bienvenidoooo");
-                alert("Bienvenido");
-                resu = 1;
-                try {
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("principal.fxml"));
-                    Parent root = loader.load();
-                    PrincipalController vistaPrincipal = loader.getController();//instancia  class
-                    Scene scene = new Scene(root);
-                    Stage stage = new Stage();
-                    stage.setScene(scene);
-                    stage.show();
-                   // Stage myStage = (Stage) this.vistaPrincipal.getScene().getWindow();
-                    //myStage.close();
-                } catch (IOException e) {
-                    Logger.getLogger(Recuperacion.class.getName()).log(Level.SEVERE, null, e);
+        if (txtUsuario.getText().equals("") || txtClave.getText().equals("")) {
+            alert("llene los campos de usuario y clave");
+        } else {
+            ResultSet rs;
+            rs = base.Consultar("SELECT * FROM ferreteria.usuarios");
+            int resu = 0;
+            while (rs.next()) {
+                if (txtUsuario.getText().equals(rs.getString("nombre")) && txtClave.getText().equals(rs.getString("clave"))) {
+                    // System.out.println("bienvenidoooo");
+                    alert("Bienvenido");
+                    resu = 1;
+                    try {
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("principal.fxml"));
+                        Parent root = loader.load();
+                        PrincipalController vistaPrincipal = loader.getController();//instancia  class
+                        Scene scene = new Scene(root);
+                        Stage stage = new Stage();
+                        stage.setScene(scene);
+                        stage.show();
+                        // Stage myStage = (Stage) this.vistaPrincipal.getScene().getWindow();
+                        //myStage.close();
+                    } catch (IOException e) {
+                        Logger.getLogger(Recuperacion.class.getName()).log(Level.SEVERE, null, e);
+                    }
                 }
             }
-        }
-        rs.close();
+            rs.close();
 
-        if(resu!=1){
-            alert("Error:   usuario no existe");
-            txtUsuario.setText(null);
-            txtClave.setText(null);
-        }
-    }
-    }
-
-/*
-    @FXML//link a ventana principal
-    public void vistaPrincipal(ActionEvent event){
-        //alert("principal");
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("principal.fxml"));
-                Parent root = loader.load();
-                PrincipalController vistaPrincipal = loader.getController();//instancia  class
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.show();
-                Stage myStage = (Stage) this.vistaPrincipal.getScene().getWindow();
-                myStage.close();
-
-            }catch (IOException e){
-                Logger.getLogger(Recuperacion.class.getName()).log(Level.SEVERE, null,e);
+            if (resu != 1) {
+                alert("Error:   usuario no existe");
+                txtUsuario.setText(null);
+                txtClave.setText(null);
             }
         }
-*/
+    }
+
     @FXML
-    void recuperacion(ActionEvent event){
-        //alert("recuperacion");
+    void recuperacion(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("recuperacion.fxml"));
             Parent root = loader.load();
@@ -98,23 +83,65 @@ public class Controller {
             stage.setScene(scene);
             stage.show();
 
-            stage.setOnCloseRequest(e-> recuperacionPass.controladorClose());// cuando se cierra ejecuta esto
+            stage.setOnCloseRequest(e -> recuperacionPass.controladorClose());// cuando se cierra ejecuta esto
             Stage myStage = (Stage) this.recuperacion.getScene().getWindow();
             myStage.close();
 
-        }catch (IOException e){
-            Logger.getLogger(Recuperacion.class.getName()).log(Level.SEVERE, null,e);
+        } catch (IOException e) {
+           // Logger.getLogger(Recuperacion.class.getName()).log(Level.SEVERE, null, e);
+            System.out.println("error en recuperacion.");
         }
     }
-    @FXML void cancelar(){
-        System.out.println("click en  Cancelar");
-        System.exit(WITDH);
-    }
-    public void alert(String msj){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setHeaderText(null);
-        alert.setTitle("ALERT");
-        alert.setContentText(msj);
-        alert.showAndWait();
-    }
+
+/*
+    @FXML
+    public void enviarCorreo(String destinatario, String claveGenerada) throws MessagingException {
+        String correo = "ferreteriagamarra812@gmail.com";
+        String pass = "ferreteria812";
+        String asunto = "Recupero de contraseña";//mando el mensaje que  se genero
+            Properties propiedad = new Properties();
+            propiedad.put("mail.smtp.auth", "true");
+            propiedad.put("mail.smtp.starttls.enable", "true");
+            propiedad.put("mail.smtp.host", "smtp.gmail.com");
+            propiedad.put("mail.smtp.port", "587");
+            propiedad.put("mail.smtp.clave", pass);
+
+            Session session = Session.getDefaultInstance(propiedad, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return super.getPasswordAuthentication();
+                }
+            });
+
+            try {
+                MimeMessage mail = new MimeMessage(session);
+                mail.setFrom(new InternetAddress(correo));
+                mail.addRecipients(Message.RecipientType.TO, String.valueOf(new InternetAddress(destinatario)));
+                mail.setSubject(asunto);
+                mail.setText("Su clave de recuperación es: " + claveGenerada);
+                Transport transport = session.getTransport("smtp");
+                transport.connect("smtp.gmail.com", correo, pass);
+                transport.sendMessage(mail, mail.getRecipients(javax.mail.Message.RecipientType.TO));
+                // transport.sendMessage(mail, mail.getAllRecipients());
+                transport.close();
+                System.out.println("Correo enviado");
+            } catch (AddressException e) {
+                System.out.println("error en enviar correo: " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }*/
+
+        @FXML
+        void cancelar() {
+            System.out.println("click en  Cancelar");
+            System.exit(WITDH);
+        }
+
+        public void alert(String msj) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText(null);
+            alert.setTitle("ALERT");
+            alert.setContentText(msj);
+            alert.showAndWait();
+        }
 }
