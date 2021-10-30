926aecc49453845a9e3517984acdd70dec972edb
albi.penayo <albi.penayo@gmail.com>
2021-10-30 01:53:15 -0300

    edit

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,10 @@ module Proyecto {
     requires javafx.controls;
     requires javafx.graphics;
     requires java.sql;
+    requires mysql.connector.java;
+    requires mail;
+    requires java.rmi;//esto no es necesario
+
     opens ferreteria;
     opens controllerPersona;
 }
